c89c68095830544bb03d9c6cab0e166b935d496e
Anggi Al Ansori <anggialansori.rpl4@gmail.com>
2023-08-02 14:39:04 +0700

    Update main.js

--- apitest/js/main.js
+++ apitest/js/main.js
@@ -3,8 +3,8 @@ fetch("http://128.199.173.229:4000/stats")
 .then(response => {
     console.log(response);
     
-    document.getElementById('quote').innerHTML = response.content;
-    document.getElementById('author').innerHTML = '- ' + response.originator.name + ' -';
+    document.getElementById('download').innerHTML = response.content;
+    document.getElementById('user').innerHTML = '- ' + response.originator.name + ' -';
 })
 .catch(err => {
 	console.log(err);
